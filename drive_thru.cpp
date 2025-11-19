085e7bf58fe4df52130217517d65f1bb2be877f2
Raven Cunanan <63638637+RavenCunanan@users.noreply.github.com>
2025-11-18 17:33:37 -0800

    Create drive_thru.cpp

---
+++ drive_thru.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+void welcome(){
+  std :: cout << "Welcome to Raven's Diner!\n";
+  std :: cout << "Here's the menu:\n";
+  std :: cout << "1. 🍔 Cheeseburger\n";
+  std :: cout << "2. 🍟 Fries\n";
+  std :: cout << "3. 🥤 Soda\n";
+  std :: cout << "4. 🍦 Ice Cream\n";
+  std :: cout << "5. 🍪 Cookie\n";
+}
+
+
+int main() {
+  welcome(); 
+}
